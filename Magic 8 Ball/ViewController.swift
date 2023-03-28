dc5812e8c8e031ded31351ca4f41c02c3a4ae9da
yusufkans <yusufkan.surmelioglu@vestel.com.tr>
2023-03-28 09:40:35 +0300

    Randomize thing, view and button

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
-
-
+    @IBOutlet weak var ImageViewOne: UIImageView!
+    
+    @IBAction func AskButtonPressed(_ sender: Any) {
+        ImageViewOne.image = UIImage(named: ballArray.randomElement()!)
+    }
 }
 
